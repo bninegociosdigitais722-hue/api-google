2916e04046a0b0362eb7b1d5917fe71c69e1de09
bninegociosdigitais722-hue <bninegociosdigitais722@gmail.com>
2026-02-06 17:00:45 +0000

    api google

--- app/consultas/page.tsx
+++
@@ -1,9 +0,0 @@
-import ConsultasClient from './ConsultasClient'
-
-export const metadata = {
-  title: 'Consultas | Radar Local',
-}
-
-export default function ConsultasPage() {
-  return <ConsultasClient />
-}
